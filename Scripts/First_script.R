e702317abdb36a3baa68b5bf7c7d15066d916aab
Lotte Niewijk <c.a.niewijk@student.rug.nl>
2023-11-13 12:21:01 +0100

    fig01 produced plus restore added

--- Scripts/First_script.R
+++ Scripts/First_script.R
@@ -3,12 +3,15 @@ library(renv)
 
 # restore the versions of the packages used to develop this project
 renv::restore()
+# remember to use pull in Git tab to get changes from github
+# load libraries
+library(tidyverse)
 
 x<-c(1,2,3,4,5)
 y<-c(1,4,3,6,9)
 data<-data.frame(x,y)
 data
 
-
-
-fig01<- 
+data |> ggplot(aes(x=x,y=y)) +
+  geom_point() +
+  geom_smooth(method='lm')
