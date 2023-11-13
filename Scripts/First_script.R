f30142d401481e02f75010d388a5a29db55a66f6
Lotte Niewijk <c.a.niewijk@student.rug.nl>
2023-11-13 12:05:35 +0100

    project initialized with renv

--- Scripts/First_script.R
+++ Scripts/First_script.R
@@ -1,1 +1,14 @@
 # my first github synced script
+library(renv)
+
+# restore the versions of the packages used to develop this project
+renv::restore()
+
+x<-c(1,2,3,4,5)
+y<-c(1,4,3,6,9)
+data<-data.frame(x,y)
+data
+
+
+
+fig01<- 
